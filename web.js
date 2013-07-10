2d73365041bc8ad074a296651947ef550ed92c5e
yajupadu <yajupadu@gmail.com>
2013-07-10 19:21:33 +0000

    reading from the file

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var message = fs.readFileSync('~/bitstarter/index.html');
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(message.toString());
 });
 
 var port = process.env.PORT || 5000;
